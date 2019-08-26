d1a7a33773656002220ad2779a93dba5797b295a
Collins Mucyo <collinsmucyo@gmail.com>
2019-08-26 13:37:55 -0500

    Wrapped applcation in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Thi is the applica tion main page</p>
+      <p>Thi is the application main page</p>
     </div>
   );
 }
