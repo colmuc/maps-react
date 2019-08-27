b242ebab296b6a02dbd45d98fe84c7b14df46d37
Collins Mucyo <collinsmucyo@gmail.com>
2019-08-27 01:51:35 -0500

    removed key

--- src/components/map.js
+++ src/components/map.js
@@ -16,7 +16,7 @@ const MyMap = () => {
     <div style={{ width: "100vw", height: "100vh" }}>
       <WrappedMap
         googleMapURL={
-          "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyC6EGOhspoNHFZ8j8bEBNOziRKDtQ3H0rg"
+          "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
         }
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
